ab23d2cf6a1d12bbdda094514fdb58c9665efaae
Alejandro Sánchez Monzón <alejandrosanchezmonzon1@gmail.com>
2022-09-13 21:30:19 +0200

    Adición de Ejercicio 2

--- src/main/kotlin/ExpresionesRegulares/Main.kt
+++ src/main/kotlin/ExpresionesRegulares/Main.kt
@@ -1,2 +1,23 @@
 package ExpresionesRegulares
 
+fun main() {
+    var isValid: Boolean
+
+    do {
+        print("Dime tu correo electrónico: ")
+        val email = readln().matches(Regex("[a-z0-9]+@[a-z]+\\.[a-z]{2,3}"))
+
+        isValid = email
+
+    } while (!isValid)
+
+    do {
+        print("Dime tu DNI: ")
+        val dni = readln().matches(Regex("""\d{8}[A-Za-z]"""))
+    } while (!dni)
+
+    do {
+        print("Dime tu número de cuenta: ")
+        val cuenta = readln().matches(Regex("""\d{16}"""))
+    } while (!cuenta)
+}
